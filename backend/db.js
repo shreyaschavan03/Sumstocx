dbee2c780f3c1b9f56700ec6056d8339cfc25662
prachisingh5671-cmd <prachisingh5671@gmail.com>
2025-09-13 00:01:27 +0530

    Edited Project

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+// Local DBeaver/Postgres DB
 export const localPool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "inventorydb",
-  password: "Secure@123",
+  password: "Pr@xchee2007", // your password
   port: 5432,
 });
-
-export const supabasePool = new Pool({
-  connectionString:
-    "postgresql://postgres:Secure@123@db.gpfesrlqguapdnvgpjrv.supabase.co:5432/postgres",
-  ssl: { rejectUnauthorized: false },
-});
-
-export default { localPool, supabasePool };
